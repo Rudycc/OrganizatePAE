8247507db78fe7cfaa0063b7a93c512d0519fc70
Rudycc <rcarcue@gmail.com>
2017-09-08 00:14:26 -0500

    Add cells to correct lists

--- src/Controllers/TaskController.java
+++ src/Controllers/TaskController.java
@@ -5,9 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
 
-import CellItems.ClassCellItems;
 import CellItems.TaskCellItems;
-import Cells.ClassListViewCell;
 import Cells.TaskListViewCell;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -21,19 +19,19 @@ import javafx.util.Callback;
 
 public class TaskController implements Initializable {
 	@FXML
-	public ListView<ClassCellItems> pastList;
+	public ListView<TaskCellItems> pastList;
 	@FXML
 	public ListView<TaskCellItems> futureList;
-	public List<ClassCellItems> pastCellItems = new ArrayList<>(40);
+	public List<TaskCellItems> pastCellItems = new ArrayList<>(40);
 	public List<TaskCellItems> futureCellItems = new ArrayList<>(40);
-	ObservableList<ClassCellItems> pastObservableList = FXCollections.observableArrayList();
+	ObservableList<TaskCellItems> pastObservableList = FXCollections.observableArrayList();
 	ObservableList<TaskCellItems> futureObservableList = FXCollections.observableArrayList();
 	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		// TODO Auto-generated method stub
 		for(int i = 0; i < 40; i++){
-			ClassCellItems pastItem = new ClassCellItems("Class" + i, "Professor " + i, "Day " + i);
+			TaskCellItems pastItem = new TaskCellItems("Task " + i, "Day " + i);
 			pastCellItems.add(pastItem);
 			TaskCellItems futureItem = new TaskCellItems("Task " + i, "Day " + i);
 			futureCellItems.add(futureItem);
@@ -42,12 +40,12 @@ public class TaskController implements Initializable {
 		futureObservableList.setAll(futureCellItems);
 		pastList.setItems(pastObservableList);
 		futureList.setItems(futureObservableList);
-		pastList.setCellFactory(new Callback<ListView<ClassCellItems>, ListCell<ClassCellItems>>() {
+		pastList.setCellFactory(new Callback<ListView<TaskCellItems>, ListCell<TaskCellItems>>() {
 
 			@Override
-			public ListCell<ClassCellItems> call(ListView<ClassCellItems> pastList) {
+			public ListCell<TaskCellItems> call(ListView<TaskCellItems> pastList) {
 				// TODO Auto-generated method stub
-				return new ClassListViewCell();
+				return new TaskListViewCell();
 			}
 			
 		});
