bff717f9f2e3b49d99c53fc74ad40571b267de6a
GiltosMx <adriantoscanop@gmail.com>
2017-09-07 19:40:55 -0500

    Adds new task dialog and titles forthe windows.

--- src/Controllers/TaskController.java
+++ src/Controllers/TaskController.java
@@ -1,17 +1,26 @@
 package Controllers;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
-
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TabPane;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 public class TaskController implements Initializable {
 	@FXML
 	public ListView<String> pastList;
 	@FXML
 	public ListView<String> futureList;
+	
+	@FXML Button btnNewTask;
 	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
@@ -24,5 +33,26 @@ public class TaskController implements Initializable {
 			futureList.getItems().add(s1);
 		}
 	}
+	
+	
+	public void createNewTask(){
+		
+		try {
+			Stage dialogStage = new Stage();
+			dialogStage.initOwner(btnNewTask.getScene().getWindow());
+			dialogStage.initModality(Modality.APPLICATION_MODAL);
+			dialogStage.setTitle("Create New Task");
+			
+			GridPane newTaskPane =  FXMLLoader.load(getClass().getResource("NewTaskDialog.fxml"));
+			
+			dialogStage.setScene(new Scene(newTaskPane));	
+			dialogStage.show();
+			
+		} catch (IOException e) {			
+			System.out.println("Error in createNewTask. StackTrace:\n");
+			e.printStackTrace();
+		}		
+	}
+	
 
 }
