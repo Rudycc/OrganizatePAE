9dc5d8bcb50dc134b6c5e35e2b255a092c838500
KiviKong <kevin_95ra@hotmail.com>
2017-09-22 12:15:03 -0500

    toggle radio buttons

--- src/Controllers/CreateNewTaskDialogController.java
+++ src/Controllers/CreateNewTaskDialogController.java
@@ -7,21 +7,30 @@ import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.stage.Stage;
 
 public class CreateNewTaskDialogController implements Initializable{
 	
 	@FXML Button btnCancel;
 	@FXML Button btnAccept;
+	@FXML RadioButton rBtnExam;
+	@FXML RadioButton rBtnLab;
+	@FXML RadioButton rBtnTask;
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		// TODO Auto-generated method stub
+		final ToggleGroup group = new ToggleGroup();
+		rBtnExam.setToggleGroup(group);
+		rBtnLab.setToggleGroup(group);
+		rBtnTask.setToggleGroup(group);
 	}
 	
 	public void btnCancelAction(){
 		Stage dialogStage = (Stage) btnCancel.getScene().getWindow();		
 		dialogStage.close();
 	}
-
+	
 }
