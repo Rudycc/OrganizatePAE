d7f322fa54d959095010edfc7e99fa2bfba77ed0
Rudycc <rcarcue@gmail.com>
2017-10-13 15:59:20 -0500

    Remove connection from main

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,9 +1,6 @@
 package application;
 
 
-import java.sql.Connection;
-
-import database.MyDBConnection;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
@@ -18,9 +15,6 @@ public class Main extends Application {
 			TabPane pane =  FXMLLoader.load(getClass().getResource("Main.fxml"));
 			primaryStage.setTitle("Organize...Yourself!");
 			primaryStage.setScene(new Scene(pane));
-			Connection conn = MyDBConnection.getConnection();
-			System.out.println(conn.isClosed());
-			conn.close();
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
