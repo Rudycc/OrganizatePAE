e8678bb6e39c4e34c713962c13c0c68a151257a6
Saúl Ponce <saul.ponce@wizeline.com>
2017-10-15 12:19:40 -0500

    Add task info dialog

--- src/controllers/TaskController.java
+++ src/controllers/TaskController.java
@@ -9,6 +9,7 @@ import cellItems.TaskCellItems;
 import cells.TaskListViewCell;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -16,6 +17,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ListView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
@@ -34,6 +36,27 @@ public class TaskController implements Initializable {
 
 	@FXML Button btnNewTask;
 	private ResourceBundle rb;
+	
+	private EventHandler<MouseEvent> cellClick = new EventHandler<MouseEvent>() {
+		@Override
+		public void handle(MouseEvent event) {
+			if (event.getClickCount() == 2) {
+				try {
+					Stage dialogStage = new Stage();
+					dialogStage.initOwner(btnNewTask.getScene().getWindow());
+					dialogStage.initModality(Modality.APPLICATION_MODAL);
+					dialogStage.setTitle(rb.getString("titleTaskInfo"));
+
+					GridPane newTaskPane =  FXMLLoader.load(getClass().getResource("TaskInfo.fxml"), rb);
+					dialogStage.setScene(new Scene(newTaskPane));
+					dialogStage.show();
+
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	};
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
@@ -48,28 +71,23 @@ public class TaskController implements Initializable {
 		futureObservableList.setAll(futureCellItems);
 		pastList.setItems(pastObservableList);
 		futureList.setItems(futureObservableList);
+		pastList.setOnMouseClicked(this.cellClick);
+		futureList.setOnMouseClicked(this.cellClick);
+		
 		pastList.setCellFactory(new Callback<ListView<TaskCellItems>, ListCell<TaskCellItems>>() {
-
 			@Override
 			public ListCell<TaskCellItems> call(ListView<TaskCellItems> pastList) {
-				// TODO Auto-generated method stub
 				return new TaskListViewCell();
 			}
-
 		});
 
 		futureList.setCellFactory(new Callback<ListView<TaskCellItems>, ListCell<TaskCellItems>>() {
-
 			@Override
 			public ListCell<TaskCellItems> call(ListView<TaskCellItems> param) {
-				// TODO Auto-generated method stub
 				return new TaskListViewCell();
 			}
-
 		});
-
 	}
-
 
 	public void createNewTask(){
 
