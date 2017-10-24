f9d14c08af145027784940f5b977f5108965db0d
KiviKong <kevin95ra@gmail.com>
2017-10-24 18:09:53 -0500

    Adds Functionality to ManageSubjectsDialog

--- src/database/SubjectDatabaseController.java
+++ src/database/SubjectDatabaseController.java
@@ -10,6 +10,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import cellItems.ClassCellItems;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
 public class SubjectDatabaseController {
 	public static List<ClassCellItems> getAllClasses(){
@@ -23,8 +25,7 @@ public class SubjectDatabaseController {
 			List<ClassCellItems> classes = new ArrayList<>();
 			while(rs.next()){
 				PreparedStatement ps = conn.prepareStatement("Select Subject_Time.* from Subject_Time "
-						+ "Join Subject_Times on Subject_Time.IdSubject_time = Subject_Times.IdSubject_time "
-						+ "where Subject_Times.IdSubject = ?");
+						+ "where Subject_Time.IdSubject = ?");
 				ps.setInt(1, rs.getInt(1));
 				ResultSet times = ps.executeQuery();
 				while(times.next()){
@@ -65,8 +66,7 @@ public class SubjectDatabaseController {
 		try{
 			conn = MyDBConnection.getConnection();
 			ps = conn.prepareStatement("Select Subject.* from Subject "
-					+ "Join Subject_Times on Subject.IdSubject = Subject_Times.IdSubject "
-					+ "Join Subject_Time on Subject_Time.IdSubject_time = Subject_Times.IdSubject_time "
+					+ "Join Subject_Time on Subject.IdSubject = Subject_Time.IdSubject "
 					+ "where Subject_Time.day = ?");
 			ps.setString(1, LocalDate.now().getDayOfWeek().name());
 			rs = ps.executeQuery();
@@ -92,5 +92,80 @@ public class SubjectDatabaseController {
 			}
 		}
 		return null;
+	}
+	
+	public static ObservableList<Integer> getAllSemesterIDs(){
+		Connection conn = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		ObservableList<Integer> ids = null;
+		try{
+			conn = MyDBConnection.getConnection();
+			ps = conn.prepareStatement("Select IDSemester from Semester");
+			rs = ps.executeQuery();
+			ids = FXCollections.observableArrayList();
+			
+			while(rs.next()){
+				ids.add(rs.getInt(1));
+			}
+		}catch(SQLException e){
+			e.printStackTrace();
+		}finally{
+			try{
+				ps.close();
+				rs.close();
+				conn.close();
+			}catch(SQLException e){
+				e.printStackTrace();
+			}
+		}
+		
+		return ids;
+	}
+	
+	public static boolean addSubject(String professor, String subject, int semester, String color, ArrayList<String> days, ArrayList<String> hours){
+		Connection conn = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		int subjectID = 0;
+		try{
+			//Inserts subject
+			conn = MyDBConnection.getConnection();
+			ps = conn.prepareStatement("Insert Into Subject(Name,ProfessorName,IDSemester,Color) Values(?,?,?,?)");
+			ps.setString(1, subject);
+			ps.setString(2, professor);
+			ps.setInt(3, semester);
+			ps.setString(4, color);
+			ps.executeUpdate();
+			
+			//Get the new subject's ID
+			ps = conn.prepareStatement("Select MAX(IDSubject) as ID From Subject");
+			rs = ps.executeQuery();
+			if(rs.next())
+				subjectID = rs.getInt("ID");
+			
+			//Inserts subject time
+			ps = conn.prepareStatement("Insert into Subject_Time(Day,Time,IDSubject) Values(?,?,?)");
+			for(int i=0; i< days.size();i++){
+				ps.setString(1, days.get(i));
+				ps.setString(2, hours.get(i));
+				ps.setInt(3, subjectID);
+				ps.executeUpdate();
+			}
+			
+		}catch(SQLException e){
+			e.printStackTrace();
+			return false;
+		}finally{
+			try{
+				ps.close();
+				rs.close();
+				conn.close();
+			}catch(SQLException e){
+				e.printStackTrace();
+				return false;
+			}
+		}
+		return true;
 	}
 }
