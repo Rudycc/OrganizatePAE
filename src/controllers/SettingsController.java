c7456905bcbcc2ad20f1a61d6d8d557e06349981
Saúl Ponce <saul.ponce@wizeline.com>
2017-10-26 19:29:17 -0500

    Use database values for language at startup

--- src/controllers/SettingsController.java
+++ src/controllers/SettingsController.java
@@ -1,14 +1,24 @@
 package controllers;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
+import application.Main;
+import database.SettingsDatabaseController;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DialogPane;
 import javafx.scene.control.Label;
 
 public class SettingsController implements Initializable {
@@ -24,9 +34,11 @@ public class SettingsController implements Initializable {
 	private Button aboutBtn;
 	@FXML
 	private Button acceptBtn;
+	private ResourceBundle rb;
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
+		this.rb = resources;
 		ObservableList<String> languages = FXCollections.observableArrayList("English", "Español");
 		langChoice.setItems(languages);
 		langChoice.getSelectionModel().selectFirst();
@@ -34,12 +46,25 @@ public class SettingsController implements Initializable {
 		themeChoice.setItems(themes);
 		themeChoice.getSelectionModel().selectFirst();
 	}
-	
+
 	public void aboutAction() {
-		System.out.println("Abouting stuff");
+		Alert alert = new Alert(AlertType.INFORMATION);
+		alert.setHeaderText(this.rb.getString("aboutDialogTitle"));
+		alert.setContentText(this.rb.getString("aboutDialogText"));
+		DialogPane dialogPane = alert.getDialogPane();
+		dialogPane.getStylesheets().add(getClass().getResource("../styles/global.css").toExternalForm());
+		alert.show();
 	}
-	
+
 	public void acceptAction() {
-		System.out.println("Accepting stuff");
+		Boolean changed = SettingsDatabaseController.setLanguage(langChoice.getSelectionModel().getSelectedItem().equals("English") ? "EN" : "ES");
+		if (changed) {
+			Alert alert = new Alert(AlertType.INFORMATION);
+			alert.setHeaderText(this.rb.getString("settDialogTitle"));
+			alert.setContentText(this.rb.getString("settDialogText"));
+			DialogPane dialogPane = alert.getDialogPane();
+			dialogPane.getStylesheets().add(getClass().getResource("../styles/global.css").toExternalForm());
+			alert.show();
+		}
 	}
 }
