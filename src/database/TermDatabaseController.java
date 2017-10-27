d8ac317ab6902dbb310ef335166ab6a5c3dc91d5
GiltosMx <adriantoscanop@gmail.com>
2017-10-26 20:04:35 -0500

    Adds functionality to manage term dialog.

---
+++ src/database/TermDatabaseController.java
@@ -0,0 +1,52 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.List;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+public class TermDatabaseController {
+	
+	public static void insertNewTerm(String startDate, String endDate, String description){
+		Connection conn = null;
+		PreparedStatement ps = null;
+		try {
+			conn = MyDBConnection.getConnection();
+			ps = conn.prepareStatement( "INSERT INTO Semester(Start_Date, End_Date, Description)"
+					+ 					"VALUES(?,?,?)");
+			ps.setString(1, startDate);
+			ps.setString(2, endDate);
+			ps.setString(3, description);			
+			ps.executeUpdate();			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}	
+	}
+	
+	public static ObservableList<String> getTerms(){
+		Connection conn = null;
+		Statement st = null;
+		ResultSet rs =null;
+		ObservableList<String> terms = null;
+		
+		try {			
+			conn = MyDBConnection.getConnection();
+			st = conn.createStatement();
+			rs = st.executeQuery("SELECT Description FROM Semester");
+			terms = FXCollections.observableArrayList();
+			
+			while(rs.next()){
+				terms.add(rs.getString(1));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return terms;
+	}
+	
+}
