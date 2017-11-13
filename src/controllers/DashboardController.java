d1297eebece52990f6be55938f74985fe7e738d5
Rudycc <rcarcue@gmail.com>
2017-11-12 23:19:41 -0600

    Add dialog to dashboard

--- src/controllers/DashboardController.java
+++ src/controllers/DashboardController.java
@@ -1,5 +1,6 @@
 package controllers;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -12,10 +13,20 @@ import database.ExamDatabaseController;
 import database.TaskDatabaseController;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 import javafx.util.Callback;
 
 public class DashboardController implements Initializable, Refreshable {
@@ -29,16 +40,60 @@ public class DashboardController implements Initializable, Refreshable {
 	ObservableList<ClassCellItems> classObservableList = FXCollections.observableArrayList();
 	ObservableList<TaskCellItems> taskObservableList = FXCollections.observableArrayList();
 	ObservableList<TaskCellItems> examObservableList = FXCollections.observableArrayList();
+	private ResourceBundle rb;
+	private Refreshable self = this;
 
+	private EventHandler<MouseEvent> taskClick = new EventHandler<MouseEvent>() {
+		@Override
+		public void handle(MouseEvent event) {
+			if (event.getClickCount() == 2) {
+				try {
+					Stage dialogStage = new Stage();
+					dialogStage.initOwner(taskList.getScene().getWindow());
+					dialogStage.initModality(Modality.APPLICATION_MODAL);
+					dialogStage.setTitle(rb.getString("titleTaskInfo"));
+					
+					FXMLLoader loader = new FXMLLoader(getClass().getResource("TaskInfo.fxml"), rb);
+
+					GridPane newTaskPane = loader.load();
+					
+					((Refresher)loader.getController()).setParent(self);
+					
+					ListView<TaskCellItems> src = (ListView<TaskCellItems>) event.getSource();
+					if (!src.getItems().isEmpty()) {
+						String title = src.getSelectionModel().getSelectedItem().getTaskName();
+						String description = src.getSelectionModel().getSelectedItem().getDescription();
+						String date = src.getSelectionModel().getSelectedItem().getDueDate().toString();
+						int taskId = src.getSelectionModel().getSelectedItem().getTaskId();
+						((Label) newTaskPane.getChildren().get(5)).setText(taskId+"");
+						((Label) newTaskPane.getChildren().get(1)).setText(title);
+						((Label) newTaskPane.getChildren().get(3)).setText(rb.getString("due") +": "+ date);
+						((TextArea) newTaskPane.getChildren().get(0)).setText(description);
+						((CheckBox) newTaskPane.getChildren().get(2)).setSelected(src.getSelectionModel().getSelectedItem().isDone());
+						
+						dialogStage.setScene(new Scene(newTaskPane));
+						dialogStage.show();
+					}
+
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	};
+	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
+		this.rb = resources;
 		classObservableList.setAll(SubjectDatabaseController.getTodayClasses());
 		taskObservableList.setAll(TaskDatabaseController.getTodayTasks());
 		examObservableList.setAll(ExamDatabaseController.getTodayExams());
 
 		todayList.setItems(classObservableList);
 		taskList.setItems(taskObservableList);
+		taskList.setOnMouseClicked(taskClick);
 		examList.setItems(examObservableList);
+		examList.setOnMouseClicked(taskClick);
 
 		todayList.setCellFactory(new Callback<ListView<ClassCellItems>, ListCell<ClassCellItems>>() {
 
